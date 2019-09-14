39aaccabbd6832d8ebfa4e1563d53863db520145
Jarno Lonardi <jarno.lonardi@gmail.com>
2019-09-15 01:35:44 +0300

    Move logging and error handling to db-client

--- src/db-client/postgres.ts
+++ src/db-client/postgres.ts
@@ -1,5 +1,7 @@
+require('dotenv').config();
 import { Pool, QueryResult } from 'pg';
 import named, { IQueryObject, PatchedClient } from 'node-postgres-named';
+import { SQLError } from '../errors/sql-error';
 import { logger } from '../utils/logger';
 
 const pool = new Pool({
@@ -14,22 +16,31 @@ const queryAsyncWithTx = async (tx: PatchedClient, queryString: string, values?:
   tx.query(queryString, values).then((res: QueryResult) => res.rows);
 
 const getQueryAsyncWithTx = (client: PatchedClient) => ({
-  queryAsync: (queryString: string, values?: IQueryObject) =>
-    queryAsyncWithTx(client, queryString, values)
+  queryAsync: (queryString: string, values?: IQueryObject) => {
+    logger.info('Add query in transaction:');
+    logger.info(queryString);
+    logger.info('Passed values:', values);
+    return queryAsyncWithTx(client, queryString, values);
+  }
 });
 
 const getConnection = async (fn: (client: IAsyncClient) => Promise<any[]>) => {
   const originalClient = await pool.connect();
   const client = named.patch(originalClient);
   try {
+    logger.info('Beginning transaction');
+
     await client.query('BEGIN');
     const result = await fn(getQueryAsyncWithTx(client));
     await client.query('COMMIT');
 
+    logger.info('Transaction completed');
     return result;
-  } catch (e) {
+  } catch (error) {
+    logger.error('Transaction failed. Rollbacking tranasction.');
     await client.query('ROLLBACK');
-    throw e;
+    logger.error(error);
+    throw new SQLError(error);
   } finally {
     client.release();
   }
@@ -38,9 +49,12 @@ const getConnection = async (fn: (client: IAsyncClient) => Promise<any[]>) => {
 export const queryRowsAsync = (queryString: string, values?: IQueryObject) =>
   getConnection((client: IAsyncClient) => client.queryAsync(queryString, values));
 
-export const queryAsync = (queryString: string, values?: IQueryObject) =>
-  getConnection((client: IAsyncClient) => client.queryAsync(queryString, values)).then(
-    rows => rows[0]
+export const queryAsync = (queryString: string, values?: IQueryObject): Promise<any> =>
+  getConnection(
+    async (client: IAsyncClient): Promise<any> => {
+      const rows = await client.queryAsync(queryString, values);
+      return rows[0];
+    }
   );
 
 pool.on('error', err => {
--- src/storage/user.operations.ts
+++ src/storage/user.operations.ts
@@ -1,6 +1,4 @@
-import { logger } from '../utils/logger';
 import { queryAsync, queryRowsAsync } from '../db-client/postgres';
-import { SQLError } from '../errors/sql-error';
 
 export interface IMessage {
   message: string;
@@ -14,82 +12,47 @@ export interface IUser {
   username: string;
 }
 
-export const getUser = async (userId: string): Promise<IUser> => {
-  try {
-    const res = await queryAsync(
-      `
+export const getUser = (userId: string): Promise<IUser> =>
+  queryAsync(
+    `
       SELECT user_id, username, picture
       FROM users
       WHERE user_id = $userId`,
-      { userId }
-    );
-    return res;
-  } catch (error) {
-    logger.error(error);
-    throw new SQLError(error);
-  }
-};
+    { userId }
+  );
 
-export const addUser = async (userId: string, picture: string): Promise<IUser> => {
-  try {
-    const res = await queryAsync(
-      `
+export const addUser = async (userId: string, picture: string): Promise<IUser> =>
+  queryAsync(
+    `
       INSERT INTO users (user_id, picture)
       VALUES ($userId, $picture)
       RETURNING user_id, username, picture`,
-      { userId, picture }
-    );
-    return res;
-  } catch (error) {
-    logger.error(error);
-    throw new SQLError(error);
-  }
-};
+    { userId, picture }
+  );
 
-export const setNickame = async (userId: string, username: string): Promise<IUser> => {
-  try {
-    const res = await queryAsync(
-      `
+export const setNickame = async (userId: string, username: string): Promise<IUser> =>
+  queryAsync(
+    `
       UPDATE users
       SET username = $username
       WHERE user_id = $userId
       RETURNING user_id, username`,
-      { userId, username }
-    );
-    return res;
-  } catch (error) {
-    logger.error(error);
-    throw new SQLError(error);
-  }
-};
+    { userId, username }
+  );
 
-export const sendMessage = async (userId: string, message: string): Promise<IMessage> => {
-  try {
-    const res = await queryAsync(
-      `
+export const sendMessage = async (userId: string, message: string): Promise<IMessage> =>
+  queryAsync(
+    `
       INSERT INTO public.messages(user_id, message)
       VALUES (
         (SELECT user_id FROM users WHERE user_id = $userId),
         $message)
       RETURNING created_at, message, message_id`,
-      { userId, message }
-    );
-    return res;
-  } catch (error) {
-    logger.error(error);
-    throw new SQLError(error);
-  }
-};
+    { userId, message }
+  );
 
-export const getMessages = async (): Promise<IMessage[]> => {
-  try {
-    const res = await queryRowsAsync(
-      `SELECT user_id, username, picture, message, message_id, messages.created_at FROM messages NATURAL JOIN users
+export const getMessages = async (): Promise<IMessage[]> =>
+  await queryRowsAsync(
+    `SELECT user_id, username, picture, message, message_id, messages.created_at FROM messages NATURAL JOIN users
       ORDER BY created_at DESC`
-    );
-    return res;
-  } catch (error) {
-    logger.error(error);
-    throw new SQLError(error);
-  }
-};
+  );
