354ebd78e07b5b937c81e8c9e3e3bd24982c7b05
Jarno Lonardi <jarno.lonardi@reaktor.com>
2022-05-23 15:27:43 +0300

    Remove asyncRoutes logic

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,13 @@
 import express, { RequestHandler } from 'express';
 import * as service from '../services/user.services';
 import { requiresAuthentication } from './middlewares';
-import { asyncRoute } from './async-routes';
 
 const router = express.Router();
 
-const requiresUsername: RequestHandler = asyncRoute(async (req, res, next) => {
+const requiresUsername: RequestHandler = async (req, res, next) => {
   const username = req.user && (await service.getUsername(req.user.id));
   return username ? next() : res.redirect('/username');
-});
+};
 
 router.use(requiresAuthentication);
 
@@ -16,27 +15,18 @@ router.get('/username', (_req, res) => res.render('username'));
 
 router.get('/chat', requiresUsername, (_req, res) => res.render('chat'));
 
-router.post(
-  '/messages',
-  asyncRoute(async (req, res) => {
-    const messages = await service.sendMessage(req.user.id, req.body.message);
-    res.json(messages);
-  })
+router.post('/messages', async (req, res) => {
+  const messages = await service.sendMessage(req.user.id, req.body.message);
+  res.json(messages);
+});
+
+router.post('/username', async (req, res, next) =>
+  (await service.setUsername(req.user.id, req.body.username)) ? res.redirect('/chat') : next()
 );
 
-router.post(
-  '/username',
-  asyncRoute(async (req, res, next) =>
-    (await service.setUsername(req.user.id, req.body.username)) ? res.redirect('/chat') : next()
-  )
-);
-
-router.get(
-  '/messages',
-  asyncRoute(async (req, res) => {
-    const messages = await service.getMessages(req.user);
-    res.json(messages);
-  })
-);
+router.get('/messages', async (req, res) => {
+  const messages = await service.getMessages(req.user);
+  res.json(messages);
+});
 
 export const userRoutes = router;
